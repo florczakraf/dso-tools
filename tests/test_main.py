1f4ae5542a6bbfd4c042aff3b433c49b123d87f5
Rafał Florczak <florczak.raf@gmail.com>
2020-09-24 18:13:18 +0200

    Add missing test for --patch-string-table

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,9 @@
+import json
 import subprocess
 
 import pytest
 
-from dso_tools.dso import DSO
+from dso_tools.dso import DSO, normalize_code
 from dso_tools.main import parse_args
 
 
@@ -44,3 +45,39 @@ def test_argument_parser():
 
     with pytest.raises(SystemExit):
         parse_args(["--dump-string-table", "--patch-string-table", "/path/to/patch/file", "/path/to/dso"])
+
+
+def test_patch_string_table(tmp_path):
+    dso = DSO()
+    dso.global_strings = [b"", b"second", b"third", b""]
+    dso.code = [
+        b"\x54",  # OP_ASSERT
+        b"\x08",  # offset for "third"
+        b"\x46",  # OP_LOADIMMED_STR
+        b"\x01",  # offset for "second"
+        b"\x00",  # to be patched in runtime
+    ]
+    dso.string_references = [(8, [4])]
+    dso_file = tmp_path / "dso_file"
+    dso_file.write_bytes(dso.encode())
+
+    patch = {1: "foo"}
+    patch_file = tmp_path / "patch_file"
+    patch_file.write_text(json.dumps(patch))
+
+    subprocess.check_call(["dso", "--patch-string-table", patch_file.as_posix(), dso_file.as_posix()])
+
+    with dso_file.open("rb") as stream:
+        new_dso = DSO.from_stream(stream)
+
+    assert new_dso.global_strings == [b"", b"foo", b"third", b""]
+    assert normalize_code(new_dso.code) == normalize_code(
+        [
+            b"\x54",  # OP_ASSERT
+            b"\x05",  # new offset for "third"
+            b"\x46",  # OP_LOADIMMED_STR
+            b"\x01",  # offset for "foo"
+            b"\x00",  # to be patched in runtime
+        ]
+    )
+    assert new_dso.string_references == [(5, [4])]
